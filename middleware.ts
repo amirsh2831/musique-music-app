2366e98acd8fd47154bdd9abc3412c4d1474e951
amirsh2831 <amirsh2831@gmail.com>
2024-12-21 18:10:11 +0330

    local dev login problem fixed

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
-// import { withAuth } from "next-auth/middleware"
-export { default } from "next-auth/middleware"
+// import { withAuth } from "next-auth/middleware";
+// // export { default } from "next-auth/middleware"
 // export default withAuth({
-//     // Matches the pages config in `[...nextauth]`
-//     pages: {
-//         signIn: "/Login",
-//         newUser: "/Signup",
-//     },
-    
-// })
-export const config = { matcher: ["/"]}
+//   // Matches the pages config in `[...nextauth]`
+//   pages: {
+//     signIn: "/Login",
+//     newUser: "/Signup",
+//   },
+// });
+// // export const config = { matcher: ["/"]}
+export default function middleware() {}
